3f4fbe2f3ce39f5ca7e626d60d263519bf536c22
SkidRow <51795604+RolandoHidalgo@users.noreply.github.com>
2022-05-28 02:48:56 -0400

    add inertia

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,20 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue'
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init()
+
+createInertiaApp({
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props, plugin }) {
+        Vue.use(plugin)
+
+        new Vue({
+            render: h => h(App, props),
+        }).$mount(el)
+    },
+})
